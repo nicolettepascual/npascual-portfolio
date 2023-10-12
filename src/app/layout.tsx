ee9a92cd4902429ec065b65789a666c11a0b19bf
Nicolette Pascual <nicolette.pascual@gmail.com>
2023-10-12 12:12:09 +0800

    chore: Suppress browser extension warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,10 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         />
       </head>
-      <body className={`${roboto.variable} ${justAnotherHand.variable}`}>
+      <body
+        suppressHydrationWarning={true}
+        className={`${roboto.variable} ${justAnotherHand.variable}`}
+      >
         {children}
       </body>
     </html>
